c195be3e01242f328d6212d67df79cd80a494876
jsingh-git <jsingh.bh@outlook.com.au>
2020-10-01 13:05:31 +1000

    Updates

---
+++ precache-manifest.a9b3fc557441d8e34ff971a74d0377f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c32ce983419ac3b6e6a52db8aa8d59",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c974f521504a3fcbb8ba",
+    "url": "/monsters/static/css/main.6c9aa1fc.chunk.css"
+  },
+  {
+    "revision": "c948f82597f515cd7ef1",
+    "url": "/monsters/static/js/2.c92c41a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.c92c41a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c974f521504a3fcbb8ba",
+    "url": "/monsters/static/js/main.401016c3.chunk.js"
+  },
+  {
+    "revision": "f299428293d851934848",
+    "url": "/monsters/static/js/runtime-main.9226b95f.js"
+  }
+]);
